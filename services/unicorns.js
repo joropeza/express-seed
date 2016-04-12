420051c9e61627d2b1608338654aa64d64e73d83
Jon Oropeza <jonoropeza@HDC-WRK-M19.local>
2016-04-11 20:49:48 -0700

    updating linting rules

--- services/unicorns.js
+++ services/unicorns.js
@@ -2,12 +2,9 @@ import Promise from 'bluebird';
 import _ from 'lodash';
 
 export function get() {
-	return new Promise(function (resolve, reject) {
-		const unicorns = [{name: 'yo'}, {name: 'matic'}];
-		const names = _.map(unicorns, (unicorn) => { 
-			return unicorn.name;
-		});
-		resolve(names);
-	});
+    return new Promise((resolve) => {
+        const unicorns = [{ name: 'yo' }, { name: 'matic' }];
+        const names = _.map(unicorns, (unicorn) => unicorn.name);
+        resolve(names);
+    });
 }
-
